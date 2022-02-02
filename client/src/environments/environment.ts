201df70afe96a8822373cf8185875b0b3dd3c484
Ruth Rivas <rhrivasg@gmail.com>
2022-02-02 04:59:47 -0400

    routine player

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,12 +1,13 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 
-import { FixedIdentityProviderBuilder } from "src/app/services/fixed-identity-provider.builder";
-import { SynchronizedIdentityProviderService } from "src/app/services/synchronized-identity-provider.service";
+import { FixedIdentityProviderBuilder } from 'src/app/services/fixed-identity-provider.builder';
+import { SynchronizedIdentityProviderService } from 'src/app/services/synchronized-identity-provider.service';
 
 // The list of file replacements can be found in `angular.json`.
-const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjZTE3NGE4Ni03MWY4LTRlY2EtYTljZC00NTk3ZGE5NTEyZDQiLCJyb2xlcyI6WyJwYXJ0aWNpcGFudCIsInRyYWluZXIiXX0.uToTRTM5AveEDaNZ4MQmCrzU4myo30bc_e3X19LjlsUs8Za3O2_4aN23c7i56mPzsWFk78LOSW2Td2CHx3fDtA",
-  fixedAuthBuilder = new FixedIdentityProviderBuilder(token)
+const token =
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIwYmQzMGE3NS1kOTlmLTRkMGYtYTY5Ni02MDAxYzExYjc3MmYiLCJyb2xlcyI6WyJwYXJ0aWNpcGFudCIsInRyYWluZXIiXX0.g8YqXQeOinYrl1h9jUp0KSagMsBJkia1ucRq_YHIy9YxzwHsbhPsJTMHOxlaU4jjuM-DTlPJpILnPIvUPmSM5g',
+  fixedAuthBuilder = new FixedIdentityProviderBuilder(token);
 
 export const environment = {
   production: false,
@@ -14,8 +15,9 @@ export const environment = {
   authApiUrl: 'http://localhost:3000',
   socialStreamingApiUrl: 'ws://localhost:5000',
   fitnessApiUrl: 'http://localhost:8080',
+  summaryApiUrl: 'http://localhost:8081',
   authInterceptor: fixedAuthBuilder.authProvider,
-  identityProviderService: SynchronizedIdentityProviderService
+  identityProviderService: SynchronizedIdentityProviderService,
 };
 
 /*
